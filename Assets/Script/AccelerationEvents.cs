6d2ed9c6109f90f71545b9909f2784bc512ff682
Rowan Shah <rowan.shah@ibm.com>
2019-08-13 12:37:42 +0100

    Low Pass Filter added

---
+++ Assets/Script/AccelerationEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationEvents : MonoBehaviour
+{ 
+    void Update()
+    {
+        GetAccelerometerValue();
+    }
+
+    Vector3 GetAccelerometerValue()
+    {
+        Vector3 acc = Vector3.zero;
+        float period = 0.0f;
+
+        foreach(AccelerationEvent evnt in Input.accelerationEvents)
+        {
+            acc += evnt.acceleration * evnt.deltaTime;
+            period += evnt.deltaTime;
+        }
+        if (period > 0)
+        {
+            acc *= 1.0f / period;
+        }
+        return acc;
+    }
+}
+
